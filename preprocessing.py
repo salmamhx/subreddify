bf99313edffa6b3e88c0064d99c03fd964260279
Jason Zheng <jasonz4200@gmail.com>
2022-04-23 17:07:07 -0400

    Finish text preprocessing
    Co-authored-by: salmamhx <salmamhx@users.noreply.github.com>
    Co-authored-by: Skyler Shapiro <skylershapiro@users.noreply.github.com>

--- preprocessing.py
+++ preprocessing.py
@@ -1,88 +1,51 @@
-# import libraries
-# import pandas as pd
-# import csv
-# from sklearn.feature_extraction.text import CountVectorizer
-# import subreddit_apis
-
-# list of each subreddit, either popular or curated
-# popular = ['askreddit', 'worldnews', 'funny', 'gaming', 'news', 'movies', 'tifu', 'mildlyinteresting', 'explainlikeimfive', 'pics', 'todayilearned', 'jokes', 'aww', 'videos', 'lifeprotips', 'twoxchromosomes', 'oldschoolcool', 'art', 'dataisbeautiful', 'amitheasshole', ]
-# curated = ['askreddit', 'news', 'gaming', 'movies', 'tifu', 'amitheasshole', 'todayilearned', 'lifeprotips', 'oldschoolcool', 'dataisbeautiful', 'explainlikeimfive', 'mildlyinteresting', 'aww', 'twoxchromosomes', 'wallstreetbets', 'politics', 'cornell', 'technews']
-
-# list of post titles for each subreddit
-# titles = {
-#     'title1',
-#     'title2'
-# }
-
 import pandas as pd
+import re
 import string
 
-PUNCTUATIONS = [string.punctuation[i] for i in range(len(string.punctuation))]
-df = pandas.read_csv('data/test_popular_data.csv')
-print(df)
+def clean_text(row):
+    # Lower case
+    row = row.lower()
 
-def preprocess(title):
-    """
-    Converts a string into a list of strings corresponding to each word. For
-    example: "Hello world!" becomes ['Hello', 'world', '!']
-    """
-    result = title.split()
-    
-    for word in result:
-        for punctuation in PUNCTUATIONS:
-            if punctuation in word:
-                i = result.index(word)
-                before_result = result[:i]
-                after_result = result[i+1:]
+    # Remove URLs
+    row = re.sub('http\S+|www.\S+', '', row)
 
-                # tmp_result is a list of just this word: eg. the 'as!sss' becomes
-                # ['a', 's', '!'];
-                i = word.index(punctuation)
-                before_word = word[:i]
-                after_word = word[i+1:]
-                word_list = filter(None, [before_word] + [punctuation] + [after_word])
-                
-                result = before_result + word_list + after_result
-        
-        # result = ['Hello', 'world!']
-        # result = ['world', '!']
-    
+    # Remove @mentions
+    row = re.sub('@[A-Za-z0-9]+', '', row)
+
+    # Remove non-standard characters
+    row = row.encode("ascii", "ignore").decode()
+
+    # Remove punctuation
+    row = row.translate(str.maketrans('', '', string.punctuation))
+
+    # Remove extraneous whitespace
+    row = row.strip()
+
+    # Split titles by words
+    row = row.split()
+
+    return row
+
+
+def all_words_subreddit(df):
+    """
+    Iterates through the given data frame and returns tokenized versions of all
+    post titles within a subreddit.
+    """
+    result = []
+    columns = list(df)[1:]
+
+    for subreddit in columns:
+        row_length = len(df[subreddit])
+        for i in range(row_length):
+            post_title = df[subreddit][i]
+            # print(post_title)
+            result.append(clean_text(post_title))
+
     return result
 
-# Iterate through all post titles in data
-all_words_popular = []
-all_words_curated = []
 
-for subreddit in popular:
-    for title in subreddit:
-        all_words_popular.append(preprocess(title))
-
-for subreddit in curated:
-    for title in subreddit:
-        all_words_curated.append(preprocess(title))
-            
-print(all_words_popular)
-            
-# df = pd.read_csv('popular_data.csv', usecols=subreddits)
-
-# # create vectorizer, extracting stop words
-# vectorizer = CountVectorizer(stop_words='english')
-
-# vectorizer.fit_transform(subreddit_apis.curated_dict).toArray()
-# vectorizer.fit_transform(subreddit_apis.popular_dict).toArray()
-
-# # gets each word in the titles
-# vectorizer.fit(titles)
-
-# # gets dictionary of above words
-# vectorizer.get_feature_names()
-
-# # each title is transformed to a vector space
-# vector = vectorizer.transform(titles)
-# vector_spaces = vector.toarray()
-# # vector_spaces
-
-# # sentences with their respective vector space representations
-# for t, v in zip(titles, vector_spaces):
-#     print(t)
-#     print(v)
+df_popular = pd.read_csv('data/test_popular_data.csv')
+df_curated = pd.read_csv('data/test_curated_data.csv')
+all_words_popular = all_words_subreddit(df_popular)
+all_words_curated = all_words_subreddit(df_curated)
